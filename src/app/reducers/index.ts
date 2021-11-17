11d6f635b408fd98665b89b819375270214fc61e
darkoputic <dputic93@gmail.com>
2021-11-17 14:55:44 +0100

    init

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -9,30 +9,14 @@ import {
 import { environment } from '../../environments/environment';
 import * as fromRouter from '@ngrx/router-store';
 
-/**
- * Every reducer module's default export is the reducer function itself. In
- * addition, each module should export a type or interface that describes
- * the state of the reducer plus any selector functions. The `* as`
- * notation packages up all of the exports into a single object.
- */
-
 import * as fromLayout from './../core/reducers/layout.reducer';
 import { InjectionToken } from '@angular/core';
 
-/**
- * As mentioned, we treat each reducer like a table in a database. This means
- * our top level state interface is just a map of keys to inner state types.
- */
 export interface State {
   [fromLayout.layoutFeatureKey]: fromLayout.State;
   router: fromRouter.RouterReducerState<any>;
 }
 
-/**
- * Our state is composed of a map of action reducer functions.
- * These reducer functions are called with each dispatched action
- * and the current or initial state and return a new immutable state.
- */
 export const ROOT_REDUCERS = new InjectionToken<ActionReducerMap<State, Action>>('Root reducers token', {
   factory: () => ({
     [fromLayout.layoutFeatureKey]: fromLayout.reducer,
@@ -54,14 +38,7 @@ export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
   };
 }
 
-/**
- * By default, @ngrx/store uses combineReducers with the reducer map to compose
- * the root meta-reducer. To add more meta-reducers, provide an array of meta-reducers
- * that will be composed to form the root meta-reducer.
- */
-export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? [logger]
-  : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [logger] : [];
 
 /**
  * Layout Selectors
